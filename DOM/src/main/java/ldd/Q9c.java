b43ee9b38f28dd6df0b7cada87903ae2a556786c
Nelson Henrique <nhsneto@gmail.com>
2023-12-15 19:20:57 -0300

    Adiciona questao 9 letra c (DOM)

---
+++ DOM/src/main/java/ldd/Q9c.java
@@ -0,0 +1,47 @@
+package ldd;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.XPathCompiler;
+import net.sf.saxon.s9api.XPathExecutable;
+import net.sf.saxon.s9api.XPathSelector;
+import net.sf.saxon.s9api.XdmNode;
+import net.sf.saxon.s9api.XdmValue;
+
+public class Q9c {
+
+    public static void main(String[] args) throws Exception {
+        Processor processor = new Processor(false);
+        net.sf.saxon.s9api.DocumentBuilder dbu = processor.newDocumentBuilder();
+        XdmNode node = dbu.build(new File("bibliography.xml"));
+        XPathCompiler xpath2 = processor.newXPathCompiler();
+        XPathExecutable exec = xpath2.compile("avg(//book[@category = 'SO']/price)");
+        XPathSelector selector = exec.load();
+        selector.setContextItem(node);
+        XdmValue average = selector.evaluate();
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document out = db.newDocument();
+        Element root = out.createElement("result");
+        root.setTextContent(average.toString());
+        out.appendChild(root);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(out);
+        StreamResult result = new StreamResult(new File("result.xml"));
+        transformer.transform(source, result);
+    }
+}
