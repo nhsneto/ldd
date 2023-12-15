ac90df199de178210e1c6a366f86dbeb4bb9530d
Nelson Henrique <nhsneto@gmail.com>
2023-12-15 17:28:03 -0300

    Adiciona questao 9 letra b (DOM)

---
+++ DOM/src/main/java/ldd/Q9b.java
@@ -0,0 +1,53 @@
+package ldd;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Q9b {
+
+    public static void main(String[] args) throws Exception {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new File("bibliography.xml"));
+
+        NodeList books = doc.getElementsByTagName("book");
+        int authorCount = 0;
+        int bookCount = 0;
+        for (int i = 0; i < books.getLength(); i++) {
+            Node book = books.item(i);
+            NodeList bookChildren = book.getChildNodes();
+            for (int j = 0; j < bookChildren.getLength(); j++) {
+                if (bookChildren.item(j).getNodeName().equals("author")) {
+                    authorCount++;
+                }
+            }
+
+            if (authorCount > 1) {
+                bookCount++;
+            }
+            authorCount = 0;
+        }
+
+        Document out = db.newDocument();
+        Element root = out.createElement("result");
+        root.setTextContent(Integer.toString(bookCount));
+        out.appendChild(root);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(out);
+        StreamResult result = new StreamResult(new File("result.xml"));
+        transformer.transform(source, result);
+    }
+}
