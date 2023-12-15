bbe59eb81339ce312a770b05e522747f0b9a1d71
Nelson Henrique <nhsneto@gmail.com>
2023-12-15 20:03:45 -0300

    Adiciona template para XPath2

---
+++ DOM/src/main/java/ldd/TemplateXPath2.java
@@ -0,0 +1,42 @@
+package ldd;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.XPathCompiler;
+import net.sf.saxon.s9api.XPathExecutable;
+import net.sf.saxon.s9api.XPathSelector;
+import net.sf.saxon.s9api.XdmNode;
+
+public class TemplateXPath2 {
+
+    public static void main(String[] args) throws Exception {
+        Processor processor = new Processor(false);
+        net.sf.saxon.s9api.DocumentBuilder dbu = processor.newDocumentBuilder();
+        XdmNode node = dbu.build(new File("bibliography.xml"));
+        XPathCompiler xpath2 = processor.newXPathCompiler();
+        XPathExecutable exec = xpath2.compile("");
+        XPathSelector selector = exec.load();
+        selector.setContextItem(node);
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document out = db.newDocument();
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(out);
+        StreamResult result = new StreamResult(new File("result.xml"));
+        transformer.transform(source, result);
+    }
+}
