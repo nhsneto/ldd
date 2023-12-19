5b87f30b6f65ae57128e8264b4fa29f12f9c14e2
Nelson Henrique <nhsneto@gmail.com>
2023-12-19 20:49:33 -0300

    Usa xpath2 ao inves de xpath na versao 1.0

--- DOM/src/main/java/ldd/Q10c.java
+++ DOM/src/main/java/ldd/Q10c.java
@@ -9,15 +9,16 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.XPathCompiler;
+import net.sf.saxon.s9api.XPathExecutable;
+import net.sf.saxon.s9api.XPathSelector;
+import net.sf.saxon.s9api.XdmNode;
 
 public class Q10c {
 
@@ -25,27 +26,21 @@ public class Q10c {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(new File("cd_catalog.xml"));
+        Processor processor = new Processor(false);
+        XdmNode node = processor.newDocumentBuilder().wrap(doc);
 
-        double highestPrice = 0d;
-        NodeList priceNodeList = doc.getElementsByTagName("price");
-        for (int i = 0; i < priceNodeList.getLength(); i++) {
-            double price = Double.parseDouble(priceNodeList.item(i).getTextContent());
-            if (highestPrice < price) {
-                highestPrice = price;
-            }
-        }
-
-        XPathFactory xpathFactory = XPathFactory.newInstance();
-        XPath xpath = xpathFactory.newXPath();
-        XPathExpression expr = xpath.compile("//cd[price = " + highestPrice + "]");
+        XPathCompiler xpath2 = processor.newXPathCompiler();
+        XPathExecutable exec = xpath2.compile("//cd[price = max(//price)]");
+        XPathSelector selector = exec.load();
+        selector.setContextItem(node);
 
         Document out = db.newDocument();
         Element root = out.createElement("result");
-        NodeList cdNodeList = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-        for (int i = 0; i < cdNodeList.getLength(); i++) {
-            Node cd = cdNodeList.item(i);
-            root.appendChild(out.importNode(cd, true));
-        }
+        selector.forEach(item -> {
+            XdmNode xdmNode = (XdmNode) item;
+            Node domNode = (Node) xdmNode.getExternalNode();
+            root.appendChild(out.importNode(domNode, true));
+        });
         out.appendChild(root);
 
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
