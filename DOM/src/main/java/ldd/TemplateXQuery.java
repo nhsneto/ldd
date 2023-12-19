2d0b0e681fb2d16754a3be358d7238994bea3c7a
Nelson Henrique <nhsneto@gmail.com>
2023-12-19 20:38:54 -0300

    Adiciona template para XQuery

---
+++ DOM/src/main/java/ldd/TemplateXQuery.java
@@ -0,0 +1,47 @@
+package ldd;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.XQueryCompiler;
+import net.sf.saxon.s9api.XQueryEvaluator;
+import net.sf.saxon.s9api.XQueryExecutable;
+import net.sf.saxon.s9api.XdmNode;
+
+public class TemplateXQuery {
+
+    public static void main(String[] args) throws Exception {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new File("cd_catalog.xml"));
+        Processor processor = new Processor(false);
+        XdmNode node = processor.newDocumentBuilder().wrap(doc);
+
+        XQueryCompiler xquery = processor.newXQueryCompiler();
+        XQueryExecutable exec = xquery.compile("");
+        XQueryEvaluator evaluator = exec.load();
+        evaluator.setContextItem(node);
+
+        Document out = db.newDocument();
+        Element root = out.createElement("result");
+        out.appendChild(root);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(out);
+        StreamResult result = new StreamResult(new File("result.xml"));
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.transform(source, result);
+    }
+}
