3f2dd2dde478ff4c7550a7c305e40a92dd61d829
Nelson Henrique <nhsneto@gmail.com>
2023-12-19 23:17:43 -0300

    Adiciona questao 10 letra g (DOM)

---
+++ DOM/src/main/java/ldd/Q10g.java
@@ -0,0 +1,53 @@
+package ldd;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.XPathCompiler;
+import net.sf.saxon.s9api.XPathExecutable;
+import net.sf.saxon.s9api.XPathSelector;
+import net.sf.saxon.s9api.XdmNode;
+
+public class Q10g {
+
+    public static void main(String[] args) throws Exception {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new File("cd_catalog.xml"));
+        Processor processor = new Processor(false);
+        XdmNode node = processor.newDocumentBuilder().wrap(doc);
+
+        XPathCompiler xpath2 = processor.newXPathCompiler();
+        XPathExecutable exec = xpath2.compile("//cd[price = min(//price)]/year");
+        XPathSelector selector = exec.load();
+        selector.setContextItem(node);
+
+        Document out = db.newDocument();
+        Element root = out.createElement("result");
+        selector.forEach(item -> {
+            XdmNode xdmNode = (XdmNode) item;
+            Node artist = (Node) xdmNode.getExternalNode();
+            root.appendChild(out.importNode(artist, true));
+        });
+        out.appendChild(root);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(out);
+        StreamResult result = new StreamResult(new File("result.xml"));
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.transform(source, result);
+    }
+}
