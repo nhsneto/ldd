58a0f71b40758fb0db7a2506fe6fcf66be502791
Nelson Henrique <nhsneto@gmail.com>
2023-12-20 00:11:12 -0300

    Adiciona questao 10 letra j (DOM)

---
+++ DOM/src/main/java/ldd/Q10j.java
@@ -0,0 +1,49 @@
+package ldd;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.XPathCompiler;
+import net.sf.saxon.s9api.XPathExecutable;
+import net.sf.saxon.s9api.XPathSelector;
+import net.sf.saxon.s9api.XdmNode;
+
+public class Q10j {
+
+    public static void main(String[] args) throws Exception {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new File("cd_catalog.xml"));
+        Processor processor = new Processor(false);
+        XdmNode node = processor.newDocumentBuilder().wrap(doc);
+
+        XPathCompiler xpath2 = processor.newXPathCompiler();
+        XPathExecutable exec = xpath2.compile("count(distinct-values(//artist))");
+        XPathSelector selector = exec.load();
+        selector.setContextItem(node);
+
+        Document out = db.newDocument();
+        Element root = out.createElement("result");
+        String count = selector.evaluateSingle().getStringValue();
+        root.setTextContent(count);
+        out.appendChild(root);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(out);
+        StreamResult result = new StreamResult(new File("result.xml"));
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.transform(source, result);
+    }
+}
